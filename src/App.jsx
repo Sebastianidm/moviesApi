96b8646d63db9f3a48424fc0de80fbd7a802073a
Sebastianidm <sebastiandiazmiranda@gmail.com>
2023-09-27 23:39:01 -0300

    Se inicia el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from '../src/pages/Home'
+import axios from 'axios';
+import YouTube from 'react-youtube';
+
+function App() {
+
+
+  return (
+    <> 
+    <Home />
+    </>
+  )
+}
+
+export default App
